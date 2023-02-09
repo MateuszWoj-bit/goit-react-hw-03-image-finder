3a43c6ce9a28447efd47c1b94800c7f6880a0ed3
Mateusz Wójcikowski <yankes90@gmail.com>
2023-02-09 22:47:37 +0100

    issue fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,9 +5,11 @@ import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
 import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
+import { key } from '../asset/pass';
 import css from './App.module.css';
 
 
+console.log(key)
 export const App = () => {
   const [query, setQuery] = useState('');
   const [page, setPage] = useState(1);
@@ -15,49 +17,51 @@ export const App = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [selectedImage, setSelectedImage] = useState(null);
-  
 
-useEffect(() => {
+  useEffect(() => {
     setIsLoading(true);
     fetch(
-      `https://pixabay.com/api/?q=${query}&page=${page}&key=6755131-7999fe22e3bb9fa8947c67297&image_type=photo&orientation=horizontal&per_page=12`
+      `https://pixabay.com/api/?q=${query}&page=${page}&key=${key}&image_type=photo&orientation=horizontal&per_page=12`
     )
       .then(response => response.json())
       .then(data => {
         setImages(prevImages => [...prevImages, ...data.hits]);
         setIsLoading(false);
+        
       })
       .catch(error => {
         setError(error);
         setIsLoading(false);
       });
-  const handleKeyDown = event => {
-    if (event.key === 'Escape') {
-      setSelectedImage(null);
-    }
-  };
-  document.addEventListener('keydown', handleKeyDown);
-  return () => {
-    document.removeEventListener('keydown', handleKeyDown);
-  };
-  
   }, [query, page]);
 
-const handleSubmit = query => {
-  setQuery(query);
-  setPage(1);
-  setImages([]);
-};
+  useEffect(() => {
+    const handleKeyDown = event => {
+      if (event.key === 'Escape') {
+        setSelectedImage(null);
+      }
+    };
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
 
-const handleLoadMore = () => {
-  setPage(prevPage => prevPage + 1);
-};
+  const handleSubmit = query => {
+    setQuery(query);
+    setPage(1);
+    setImages([]);
+  };
+
+  const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
+  };
 
   const handleImageSelection = imageUrl => {
     setSelectedImage(imageUrl);
   };
- 
-  const closeModal = () => {
+
+  const handleCloseModal = () => {
     setSelectedImage(null);
   };
 
@@ -77,9 +81,8 @@ const handleLoadMore = () => {
         </>
       )}
       {selectedImage && (
-        <Modal imageUrl={selectedImage} closeModal={closeModal} />
+        <Modal imageUrl={selectedImage} onCloseModal={handleCloseModal} />
       )}
     </div>
   );
 };
-
